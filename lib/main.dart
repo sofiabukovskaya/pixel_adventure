ba9e2d430eba3e2541dc46f629ffa2914c8ea7a3
sofiabukovskaya <sofi454545@gmail.com>
2026-04-22 15:58:07 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+void main() {
+  runApp(GameWidget(game: PixelAdventure(),),);
+}
