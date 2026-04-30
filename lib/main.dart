e55aab493deedb37d33f01735e46d0e05c5ac126
sofiabukovskaya <sofi454545@gmail.com>
2026-04-30 17:05:19 +0200

    add Level class and tiled map support
    - Implement `Level` class using `TiledComponent` to load map files.
    - Configure `PixelAdventure` game with a `CameraComponent` and fixed resolution.
    - Set up project to support Tiled maps, including `.tmx` and `.tsx` assets.
    - Update `main.dart` to initialize Flame with full-screen and landscape orientation.
    - Register asset paths for tiles and terrain images in `pubspec.yaml`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 void main() {
-  runApp(GameWidget(game: PixelAdventure(),),);
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+  
+  runApp(GameWidget(game: PixelAdventure()));
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,23 @@
-import 'package:flame/game.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  late final CameraComponent cam;
+
+  @override
+  Color backgroundColor() => Color(0xFF211F30);
+  @override
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(width: 640, height: 360, world: world);
+    cam.viewfinder.anchor = Anchor.topLeft;
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
